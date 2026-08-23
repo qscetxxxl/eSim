3ff38bf9d69cf06fa86cc1a68383f9c802291d13
qscetxxxl <janu10122006@gmail.com>
2026-08-23 23:36:03 +0530

    fix(ubuntu-25.04): resolve installation script errors

--- Ubuntu/install-eSim.sh
+++ Ubuntu/install-eSim.sh
@@ -59,13 +59,10 @@ run_version_script() {
     if [[ -f "$SCRIPT" ]]; then
         echo "Running script: $SCRIPT $ARGUMENT"
         bash "$SCRIPT" "$ARGUMENT"
-    echo "[eSim]" >> $config_dir/$config_file
-    echo "eSim_HOME = $eSim_Home" >> $config_dir/$config_file
-    echo "LICENSE = %(eSim_HOME)s/LICENSE" >> $config_dir/$config_file
-    echo "KicadLib = %(eSim_HOME)s/library/kicadLibrary.tar.xz" >> $config_dir/$config_file
-    echo "IMAGES = %(eSim_HOME)s/images" >> $config_dir/$config_file
-    echo "VERSION = %(eSim_HOME)s/VERSION" >> $config_dir/$config_file
-    echo "MODELICA_MAP_JSON = %(eSim_HOME)s/library/ngspicetoModelica/Mapping.json" >> $config_dir/$config_file
+    else
+    	echo "Installer script not found: $SCRIPT"
+    	exit 1
+    fi
    
 }
 
